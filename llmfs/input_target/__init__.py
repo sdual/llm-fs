d9ae1de8ff7154581273cab6e25ae9e6c33a1246
sdual <shr.nrht.0623@gmail.com>
2024-11-04 00:45:15 +0900

    add code to create input target

---
+++ llmfs/input_target/__init__.py
@@ -0,0 +1,3 @@
+from llmfs.input_target.input_target import create_input_target
+
+__all__ = ["create_input_target"]
---
+++ main.py
@@ -0,0 +1,9 @@
+from llmfs.input_target import create_input_target
+
+
+def main():
+    create_input_target()
+
+
+if __name__ == "__main__":
+    main()
